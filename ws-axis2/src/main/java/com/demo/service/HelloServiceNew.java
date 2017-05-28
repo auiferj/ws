997d542b45517b92cb48b241fcc4704f08a1a520
auiferj <auiferj@163.com>
2017-05-28 22:41:26 +0800

    first commit

---
+++ ws-axis2/src/main/java/com/demo/service/HelloServiceNew.java
@@ -0,0 +1,18 @@
+package com.demo.service;
+
+public class HelloServiceNew {
+	public String sayHelloNew() {
+		return "hello";
+	}
+
+	public String sayHelloToPersonNew(String name) {
+		if (name == null) {
+			name = "nobody";
+		}
+		return "hello," + name;
+	}
+
+	public void updateData(String data) {
+		System.out.println(data + " 已更新。");
+	}
+}
